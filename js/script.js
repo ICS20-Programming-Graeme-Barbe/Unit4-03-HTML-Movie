8b4b0b45a7e94f56ac5b780c258baff45547f852
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-05-03 15:08:15 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Created by Graeme
+// Created on: May 3
+// JS functions for index.html
+
+
+// This function tells the user what age catagorie of movies thay can see
+'use strict';
+function submit() {
+	// Declares variable
+	let userAge = document.getElementById('age').value
+	
+	// If statement for user age, what age catagorie of movies thay can see
+	if (userAge == "") {
+		// Nothing was inputed
+		document.getElementById('rating').innerHTML = "You need to input your age";
+	} else if (userAge >= 17) {
+		//If user can see a R rated movie 
+		document.getElementById('rating').innerHTML = "You can go see an R rated movie with no supervision";
+	} else if (userAge >= 13) {
+		//If user can see a PG-13 movie 
+		document.getElementById('rating').innerHTML = "You can go see an PG-13 movie with no supervision";
+	} else if (userAge >= 10) {
+		//If user can see a PG or G movie 
+		document.getElementById('rating').innerHTML = "You can go see an PG or a G movie with no supervision";
+	} else if (userAge < 10 && userAge > 0) {
+		//User needs supervision
+		document.getElementById('rating').innerHTML = "You need supervision to watch any age catagorie of movie";
+	} else {
+		//User age is too young
+		document.getElementById('rating').innerHTML = "That's not a real age, you have to be older then 0";
+	}
+}
